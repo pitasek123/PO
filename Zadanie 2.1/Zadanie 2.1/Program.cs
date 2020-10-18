2071847ef4a9d11d8274957b7a224aba0556f33b
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-10-18 12:53:43 +0200

    new

---
+++ Zadanie 2.1/Zadanie 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadanie_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+            Console.WriteLine("Podaj temp. w stopniach Celsjusza: ");
+            C = double.Parse(Console.ReadLine());
+            F = 32 + (1.8 * C);
+            Console.WriteLine(F);
+            Console.ReadKey();
+        }
+    }
+}
