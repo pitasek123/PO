7ab6b7e8ad2935b8f5a741128d9e929aa16dd45b
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-10-26 18:39:02 +0100

    add3

---
+++ Zadanie 2.3/Zadanie 2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double masa, wzrost, bmi;
+            Console.WriteLine("Podaj masę w kilogramach: ");
+            masa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost w metrach: ");
+            wzrost = float.Parse(Console.ReadLine());
+            bmi = masa / wzrost;
+            Console.WriteLine("Twoje BMI wynosi: ");
+            Console.WriteLine(bmi);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
