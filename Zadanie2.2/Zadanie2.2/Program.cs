309f81ede7f067ec722b312fd5374a77950ec172
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-10-26 18:21:31 +0100

    add2

---
+++ Zadanie2.2/Zadanie2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta;
+
+            Console.WriteLine("Podaj a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj c: ");
+            c = double.Parse(Console.ReadLine());
+            delta = b * b - (4 * a * c);
+            Console.WriteLine("Delta wynosi: ");
+            Console.WriteLine(delta);
+            Console.ReadKey();
+        }
+    }
+}
