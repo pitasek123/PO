7cac14ef78d573544a8d25cddeaa78bfa0b66128
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-11-08 13:36:22 +0100

    add3.2

---
+++ Zad3.2/Zad3.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zad3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Podaj a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (b == 0)
+                Console.WriteLine("Nie można dzielić przez 0!");
+            else
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine("Liczba jest dzielnikiem!");
+                }
+                else
+                    Console.WriteLine("Liczba nie jest dzielniiem!");
+            }
+        }
+    }
+}
