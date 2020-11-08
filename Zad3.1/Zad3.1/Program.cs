2752ff9eaee48510e7e5986979cb2d8929525f58
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-11-08 13:20:53 +0100

    add 3.1

---
+++ Zad3.1/Zad3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zad3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rok;
+            Console.WriteLine("Podaj rok do sprawdzenia: ");
+            rok = int.Parse(Console.ReadLine());
+
+            if ((rok % 4 == 0) && (rok % 100 != 0))
+            {
+                Console.WriteLine("Rok jest przestępny!");
+            }
+            else if(rok % 400 == 0)
+            {
+                Console.WriteLine("Rok jest przestępny!");
+            }
+            else
+            {
+                Console.WriteLine("Rok nie jest przestępny!");
+            }
+        }
+    }
+}
