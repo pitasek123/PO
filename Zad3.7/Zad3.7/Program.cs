076f3d8ace6f9ba481b0755b764d3e700c30861b
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-11-14 11:57:04 +0100

    add

---
+++ Zad3.7/Zad3.7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zad3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dzien=0;
+            dzien = int.Parse(Console.ReadLine());
+
+            switch(dzien)
+            {
+                case 1:
+                    Console.WriteLine("Podany dzień to Poniedziałek!");
+                    break;
+                case 2:
+                    Console.WriteLine("Podany dzień to Wtorek!");
+                    break;
+                case 3:
+                    Console.WriteLine("Podany dzień to Środa!");
+                    break;
+                case 4:
+                    Console.WriteLine("Podany dzień to Czwartek!");
+                    break;
+                case 5:
+                    Console.WriteLine("Podany dzień to Piątek!");
+                    break;
+                case 6:
+                    Console.WriteLine("Podany dzień to Sobota!");
+                    break;
+                case 7:
+                    Console.WriteLine("Podany dzień to Niedziela!");
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiego numeru!");
+                    break;
+
+                
+            }
+        }
+    }
+}
