2e0f66dcec7d4b8c22ee511b4d90894421c7b37f
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-11-14 10:55:49 +0100

    add3.5

---
+++ Zad3.5/Zad3.5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zad3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x1, x2, x0, delta;
+
+            Console.WriteLine("Podaj a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj c: ");
+            c = double.Parse(Console.ReadLine());
+            delta = b * b - (4 * a * c);
+
+            if (delta > 0)
+            {
+                delta = Math.Sqrt(delta);
+                x1 = (-b - delta) / (2 * a);
+                x2 = (-b + delta) / (2 * a);
+                Console.WriteLine("Są dwa pierwiastki równania:{0} oraz {1} ",x1, x2);
+
+            }
+            else if (delta == 0)
+            {
+                x0 = -b / (2 * a);
+                Console.WriteLine("Jest jeden pierwiastek x0:{0} ", x0);
+            }
+            else
+                Console.WriteLine("Brak pierwiastków!");
+        }
+    }
+}
