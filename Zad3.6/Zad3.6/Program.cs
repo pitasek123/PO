c8be8c3ed95165f5ed4e986c2ec6628644a4c4e8
Piotrek <59134467+pitasek123@users.noreply.github.com>
2020-11-14 11:24:16 +0100

    add

---
+++ Zad3.6/Zad3.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zad3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double masa, wzrost, bmi;
+            Console.WriteLine("Podaj masę w kilogramach: ");
+            masa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost w metrach: ");
+            wzrost = float.Parse(Console.ReadLine());
+            bmi = masa / wzrost;
+
+            if (bmi < 18.5)
+                Console.WriteLine("Niedowaga!");
+            else if (bmi >= 18.5 && bmi < 25)
+                Console.WriteLine("Waga prawidłowa!");
+            else
+                Console.WriteLine("Nadwaga!");
+            
+        }
+    }
+}
